d663bfca20f4c75af1f0ef5918ab8c4c97baf4e9
Martin Johnsson <sorill@gmail.com>
2021-11-11 09:48:58 +0100

    Filtering based on the number of markers disagree with each marker about genetic order

--- R/ma2015_make_tables.R
+++ R/ma2015_make_tables.R
@@ -47,6 +47,83 @@ lifted_map_positions <- lifted_map_positions[order(lifted_map_positions$chr,
 lifted_map_chr <- split(lifted_map_positions, lifted_map_positions$chr)
 
 
+
+## Filter map by removing markers that have flipped cM order relative to many
+## other markers
+
+is_cM_ordered <- function(map_chr) {
+    
+    genetic_orders <- map(map_chr,
+                          function(chr) order(chr$position_cM))
+    
+    is_ordered <- map_lgl(genetic_orders,
+                          function(o) identical(o, 1:length(o)))
+    
+    all(is_ordered)
+}
+
+
+## Count the number of other markers that are on the "wrong side" of each marker
+## based on their genetic positions; this gives a score of how many other markers
+## disagree with a particular marker.
+
+get_marker_flip_count_chr <- function(chr) {
+    
+    n_markers <- nrow(chr)
+    n_flipped <- numeric(n_markers)
+    
+    for (marker_ix in 1:n_markers) {
+        distance <- chr$position_cM - chr$position_cM[marker_ix]
+        n_flipped_before <- sum(distance[1:marker_ix] > 0)
+        n_flipped_after <- sum(distance[(marker_ix + 1):n_markers] < 0)
+        if (is.na(n_flipped_after)) {
+            n_flipped_after <- 0
+        }
+        n_flipped[marker_ix] <- n_flipped_before + n_flipped_after
+    }
+    n_flipped
+}
+
+
+## Filter a map by removing, for each chromosome, the markers that have the
+## highest number of disagreements with other markers. If several have the same
+## number all of those are removed.
+
+filter_marker_flip <- function(map_chr) {
+    
+    flip_counts <- map(map_chr, get_marker_flip_count_chr)
+    
+    n_chr <- length(map_chr)
+    
+    for (chr_ix in 1:n_chr) {
+        if (any(flip_counts[[chr_ix]] > 0)) {
+            max_flip <- max(flip_counts[[chr_ix]])
+            map_chr[[chr_ix]] <- map_chr[[chr_ix]][flip_counts[[chr_ix]] < max_flip,]
+        }
+    }
+    map_chr
+}
+
+
+## Iteratively remove the markers that disagree the most until no further
+## disagreements about marker order remain.
+
+iterative_filter_marker_flip <- function(map_chr) {
+
+    k <- 1
+    
+    while (!is_cM_ordered(map_chr)) {
+        print(k)
+        map_chr <- filter_marker_flip(map_chr)   
+        k <- k + 1
+    }
+    
+    map_chr
+}
+
+lifted_map_chr_flip_filter <- iterative_filter_marker_flip(lifted_map_chr)
+
+
 ## Check how far each interval has been lifted, compared to the median distance
 
 get_distance_lifted <- function(chr) {
