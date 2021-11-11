b455032f61e4fc7261bfe5d668bd52ff78c6e95b
Martin Johnsson <sorill@gmail.com>
2021-11-11 15:58:10 +0100

    Right author name; window bed files

--- R/ma2015_prepare_bed.R
+++ R/ma2015_prepare_bed.R
@@ -1,34 +1,82 @@
-## Prepare Dryad table from Li et al. (2016) for lifting
+## Prepare Dryad table from Ma et al. (2015) for lifting
 
 library(dplyr)
 library(purrr)
 library(readr)
 
+source("R/helper_functions.R")
 
-li2016 <- read_delim("data/dryad_data_cattle_rmap_Li.txt",
+
+ma2015 <- read_delim("data/dryad_data_cattle_rmap_Ma2015.txt",
                      delim = " ")
 
-li2016_chr <- split(li2016, li2016$Chr)
 
-haldane_cM <- function(r) -50 * log(1 - 2 * r)
+
+
+## Marker positions
+
+bed <- data.frame(chr = paste("chr", ma2015$Chr, sep = ""),
+                  start = ma2015$Location - 1,
+                  end = ma2015$Location,
+                  name = ma2015$Name,
+                  stringsAsFactors = FALSE)
+
+
+write.table(bed,
+            file = "outputs/ma2015_windows.bed",
+            col.names = FALSE,
+            row.names = FALSE,
+            quote = FALSE)
+
+
+
+
+
+## Marker intervals
+
+ma2015_chr <- split(ma2015, ma2015$Chr)
 
 get_marker_itervals <- function(chr) {
     
     window_start <- chr$Location[-nrow(chr)]
     window_end <- chr$Location[-1]
+    window_end_marker <- chr$Name[-1]
     window_length_bp <- window_end - window_start
     average_r <- (chr$map_f[-nrow(chr)] + chr$map_m[-nrow(chr)])/2
     window_length_cM <- haldane_cM(average_r)
+    window_end_position_cM <- cumsum(window_length_cM)
     
-    data.frame(chr = unique(chr$Chr),
+    data.frame(chr = paste("chr", unique(chr$Chr), sep = ""),
                start = window_start,
                end = window_end,
                window_length_bp = window_length_bp,
                window_length_cM = window_length_cM,
+               window_end_marker,
+               window_end_position_cM,
                rec_rate = window_length_cM/window_length_bp * 1e6)
 }
 
 
-windows <- map_dfr(li2016_chr, get_marker_itervals)
+windows <- map_dfr(ma2015_chr, get_marker_itervals)
+
+windows$window_id <- make_window_id(windows)
+
+write.table(windows,
+            file = "outputs/ma2015_windows.txt",
+            sep = "\t",
+            quote = FALSE,
+            row.names = FALSE,
+            col.names = TRUE)
 
 
+windows_bed <- data.frame(chr = windows$chr,
+                          start = windows$start - 1,
+                          end = windows$end,
+                          name = windows$window_id)
+
+
+write.table(windows_bed,
+            file = "outputs/ma2015_windows.bed",
+            col.names = FALSE,
+            row.names = FALSE,
+            quote = FALSE)
