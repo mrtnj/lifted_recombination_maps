ea9d9196640698dd4ce8e0668458156be6ac55f7
Martin Johnsson <sorill@gmail.com>
2024-05-31 12:55:32 +0200

    Replace Ma 2015

---
+++ R/brekke2023_make_tables.R
@@ -0,0 +1,68 @@
+## Make output tables for Brekke et al. (2023)
+
+library(assertthat)
+library(dplyr)
+library(readxl)
+library(ggplot2)
+library(patchwork)
+library(purrr)
+library(readr)
+
+source("R/helper_functions.R")
+
+
+
+positions <- read_excel("data/all_chr_linkage_map_JDS.xls")
+
+positions$sex_average <- (positions$male_cM + positions$female_cM) * 0.5
+
+
+
+## Create map file (single file format)
+
+map_file <- tibble(marker = positions$snpid,
+                   chr = paste("chr", positions$chr, sep = ""),
+                   position_bp = positions$Mb,
+                   position_cM = positions$sex_average)
+
+write.table(map_file,
+            file = "outputs/brekke2023_ARS-UCD1.2.txt",
+            sep = "\t",
+            quote = FALSE,
+            row.names = FALSE,
+            col.names = TRUE)
+
+
+
+
+
+## Create map file (SHAPEIT2 format)
+
+# windows_lifted_filtered_chr <- split(windows_lifted_filtered,
+#                                      windows_lifted_filtered$chr)
+# 
+# 
+# make_shapeit_map <- function(windows) {
+#     tibble(pos = windows$end,
+#            rate = windows$rec_rate,
+#            cM = windows$window_end_position_cM)
+# }
+# 
+# 
+# system("mkdir outputs/brekke2023_shapeit_ARS-UCD1.2")
+# 
+# for (chr_ix in 1:length(windows_lifted_filtered_chr)) {
+#     write.table(make_shapeit_map(windows_lifted_filtered_chr[[chr_ix]]),
+#                 file = paste("outputs/brekke2023_shapeit_ARS-UCD1.2/",
+#                              names(windows_lifted_filtered_chr)[chr_ix],
+#                              ".txt",
+#                              sep = ""),
+#                 row.names = FALSE,
+#                 col.names = FALSE,
+#                 quote = FALSE,
+#                 sep = "\t")
+# }
+# 
+# 
+# 
+# 
